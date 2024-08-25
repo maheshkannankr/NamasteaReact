fd99e1736c3e5638776a0bdd3643ed9ccd585b2a
Mahesh Kannan <maheshkannank@koraltech.in>
2024-08-25 14:30:37 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,62 +1,13 @@
-const heading = [
-  React.createElement(
-    'div',
-    {
-      id: 'parent',
-    },
-    React.createElement(
-      'div',
-      {
-        id: 'child',
-      },
-      [
-        React.createElement(
-          'h1',
-          {
-            id: 'heading',
-          },
-          'Hello World inside Child'
-        ),
-        React.createElement(
-          'h1',
-          {
-            id: 'heading',
-          },
-          'Hello World inside Child'
-        ),
-      ]
-    )
-  ),
-  React.createElement(
-    'div',
-    {
-      id: 'parent',
-    },
-    React.createElement(
-      'div',
-      {
-        id: 'child',
-      },
-      [
-        React.createElement(
-          'h1',
-          {
-            id: 'heading',
-          },
-          'Hello World inside Child'
-        ),
-        React.createElement(
-          'h1',
-          {
-            id: 'heading',
-          },
-          'Hello World inside Child'
-        ),
-      ]
-    )
-  ),
-];
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('rootss'));
-console.log(root);
-root.render(heading);
+const jsxHeading = (
+  <div id='parent'>
+    <div id='child'>
+      <h1 id='heading'>Hello World inside Child😀</h1>
+    </div>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading);
