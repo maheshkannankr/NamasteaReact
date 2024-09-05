8b320e3946733c7101a8da841ab4befab1265c71
Mahesh Kannan <maheshkannank@koraltech.in>
2024-09-05 07:47:41 +0530

    upto git episode-04

--- App.js
+++ App.js
@@ -1,13 +1,187 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './assets/fonts/font.css';
+import { RatingStar } from './assets/svgs';
 
-const jsxHeading = (
-  <div id='parent'>
-    <div id='child'>
-      <h1 id='heading'>Hello World inside Child😀</h1>
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='h_logo'>
+        <img
+          src={
+            'https://images-platform.99static.com//AxPb2VxF3L3s3BOBmw5t45Q8hfo=/269x28:978x737/fit-in/500x500/99designs-contests-attachments/117/117852/attachment_117852994'
+          }
+        />
+      </div>
+      <div className='h_navItemsContainer'>
+        <ul className='h_navItems'>
+          <li>
+            <a className='font_r header_item'>Home</a>
+          </li>
+          <li>
+            <a className='font_r header_item'>About Us</a>
+          </li>
+          <li>
+            <a className='font_r header_item'>Products</a>
+          </li>
+          <li>
+            <a className='font_r header_item'>Contact Us</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
+const RestCard = ({ ...props }) => {
+  return (
+    <div className='rest_card_layout'>
+      <img
+        className='restCard_img_style'
+        src={
+          'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/' +
+          props.src
+        }
+      />
+      <div className='restCard_detailsContainer'>
+        <h6 className='restCard_restName_text'>{props.restName}</h6>
+        <div className='restCard_ratingContainer'>
+          <RatingStar />
+          <label className='restCard_rating_text'>{props.rating}</label>
+        </div>
+        <label className='restCard_cuisine_text'>{props.cuisine}</label>
+        <label className='restCard_rating_text'>{props.locality}</label>
+      </div>
+    </div>
+  );
+};
+
+const restList = [
+  {
+    id: '579525',
+    name: 'Sri Sakthi Mess',
+    cloudinaryImageId: 'ewshoqa9wdjhigg6pho5',
+    locality: 'Vaikkal Road',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹200 for two',
+    cuisines: ['South Indian'],
+    avgRating: 3.3,
+  },
+  {
+    id: '547156',
+    name: 'Sachitha Nanthini Mess',
+    cloudinaryImageId: 'qidspzontxonxliy3ub4',
+    locality: 'Yakoop Street',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹200 for two',
+    cuisines: ['South Indian', 'Chinese'],
+    avgRating: 4.5,
+  },
+  {
+    id: '916626',
+    name: 'Garden Restaurant',
+    cloudinaryImageId:
+      'RX_THUMBNAIL/IMAGES/VENDOR/2024/6/27/1192d919-7f8c-4cea-962c-180a8d599ff8_916626.jpg',
+    locality: 'Modachur Rd',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹200 for two',
+    cuisines: ['South Indian', 'Indian'],
+    avgRating: 4.6,
+  },
+  {
+    id: '498262',
+    name: 'Yazh Restaurant',
+    cloudinaryImageId: 'jg8ptjemzkktawhi8c62',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹300 for two',
+    cuisines: ['Chinese', 'South Indian'],
+    avgRating: 3.8,
+  },
+  {
+    id: '555188',
+    name: 'Pandian Mess',
+    cloudinaryImageId: 'wnn7npcw3rxoaajl4nbz',
+    locality: 'Modachur Road',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹200 for two',
+    cuisines: ['South Indian'],
+    avgRating: 4.3,
+  },
+  {
+    id: '902431',
+    name: 'Jayasai Fruits',
+    cloudinaryImageId:
+      'RX_THUMBNAIL/IMAGES/VENDOR/2024/5/31/ba495002-7e20-40d0-b0cc-2f9d200d088c_902431.jpg',
+    locality: 'Ther Veedhi',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹200 for two',
+    cuisines: ['Juices', 'Snacks', 'Fast Food'],
+    avgRating: 5,
+  },
+  {
+    id: '726902',
+    name: 'Town Biriyani Hotel',
+    cloudinaryImageId: 'b14cd9fc40129fcfb97aa7e621719d07',
+    locality: 'Main Road',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹250 for two',
+    cuisines: ['Indian', 'Chinese', 'Snacks'],
+    avgRating: 4.1,
+  },
+  {
+    id: '497829',
+    name: 'Chill Bite',
+    cloudinaryImageId: 'ga8xwhtmilmpfiri7laz',
+    locality: 'New Hospital Road',
+    areaName: 'Gobichettipalayam',
+    costForTwo: '₹500 for two',
+    cuisines: ['Italian', 'Pizzas'],
+    avgRating: 4,
+  },
+];
+
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className='b_search_container'></div>
+      <div className='b_restcard_container'>
+        {restList.map(
+          ({
+            name,
+            avgRating,
+            cloudinaryImageId,
+            areaName,
+            cuisines,
+            costForTwo,
+          }) => {
+            return (
+              <RestCard
+                src={cloudinaryImageId}
+                restName={name}
+                rating={avgRating}
+                cuisine={cuisines.join(',')}
+                locality={areaName}
+              />
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+};
+const Footer = () => {
+  return <div className='footer'></div>;
+};
+
+const AppLayout = () => {
+  return (
+    <div className='appContainer'>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+root.render(<AppLayout />);
